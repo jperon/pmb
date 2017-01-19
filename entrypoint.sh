11126c87d18a33a01bdec5d1a0379df3ae770591
Jacques Peron <cataclop@hotmail.com>
2017-01-19 20:19:26 +0100

    Correction
    Il semble que bash aime les définitions de fonctions avant leur appel…

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-service mysql start || initialiser_db
-service php5-fpm start
-nginx -g 'daemon off;'
-
 function initialiser_db {
 	mysql_install_db
 	service mysql start
 	echo "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'; GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;" | mysql -uroot
 }
+
+service mysql start || initialiser_db
+service php5-fpm start
+nginx -g 'daemon off;'
