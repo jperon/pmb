2077f4085ed4e431bbced1e46c463515b6386809
Jacques Peron <cataclop@hotmail.com>
2017-01-21 13:42:20 +0100

    Typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ function initialiser_parametres {
 	mkdir /etc/pmb
 	touch /etc/pmb/db_param.inc.php
 	chown www-data:www-data /etc/pmb/db_param.inc.php
-	ln -s /etc/pmb/db_param.inc.php /var/www/html/pmb/includes/db_param.inc.php)
+	ln -s /etc/pmb/db_param.inc.php /var/www/html/pmb/includes/db_param.inc.php
 }
 
 ls /var/www/html/pmb/includes/db_param.inc.php || initialiser_parametres
